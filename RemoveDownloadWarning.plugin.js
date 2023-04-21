bec25b3e28b9837b957fd15edb0cc72611872b58
Eject37 <57284098+Eject37@users.noreply.github.com>
2023-04-22 01:39:14 +0300

    Add files via upload

---
+++ RemoveDownloadWarning.plugin.js
@@ -0,0 +1,29 @@
+/**
+ * @name RemoveDownloadWarning
+ * @version 1.0
+ * @description RemoveDownloadWarning
+ * @author Eject
+ *  
+ */
+
+module.exports = class Example {
+
+	load() { }
+	start() {
+
+		setInterval(function () {
+			document.querySelectorAll('[role=button][rel="noreferrer noopener"][href*=cdn]').forEach(x => {
+				if (!x.rel.includes('removed')) {
+					x.addEventListener('click', e => {
+						e.preventDefault()
+						document.querySelectorAll('.layerContainer-2lfOPe').forEach(x => x.remove())
+						window.open(x.href)
+					})
+					x.rel += ' removed'
+				}
+			})
+		}, 1000)
+
+	}
+	stop() { }
+}
